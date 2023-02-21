55ed9b8cf22c8c6718813ab70ef0e8621d3498f9
BOSS1292 <122000992+Ashraf1292@users.noreply.github.com>
2023-02-21 16:43:40 +0700

    Add files via upload

--- code/signup.php
+++ code/signup.php
@@ -17,27 +17,38 @@
     }
 
     // Check if the form has been submitted
-    // Check if the form has been submitted
-if(isset($_POST['submit'])) {
-    // Get the form data
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
+    if(isset($_POST['submit'])) {
+        // Get the form data
+        $candidate_id = $_POST['candidate_ID'];
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
 
-    // Query to save the email and password in the database
-    $sql = "INSERT INTO student (student_email, student_password) VALUES ('$email', '$pass')";
-   
-    // Execute the query
-    if ($conn->query($sql) === TRUE) {
-        // Set the user's email in the session
-        $_SESSION['email'] = $email;
-        
-        // Redirect the user to the welcome page
-        header("Location: welcomeSignin.php");
-        exit();
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // Query to check if the candidate ID exists in the database
+        $check_query = "SELECT * FROM Candidate WHERE Candidate_ID = '$candidate_id'";
+
+        // Execute the query
+        $check_result = $conn->query($check_query);
+
+        // Check if the candidate ID exists in the database
+        if ($check_result->num_rows > 0) {
+            // Query to save the email and password in the database
+            $insert_query = "INSERT INTO login (Email, Password) VALUES ('$email', '$pass')";
+            
+            // Execute the query
+            if ($conn->query($insert_query) === TRUE) {
+                // Set the user's email in the session
+                $_SESSION['email'] = $email;
+
+                // Redirect the user to the welcome page
+                header("Location: welcomeSignin.php");
+                exit();
+            } else {
+                echo "Error: " . $insert_query . "<br>" . $conn->error;
+            }
+        } else {
+            echo "Invalid Candidate ID";
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
@@ -48,8 +59,7 @@ if(isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="login2.css">
 </head>
 
 <body>
@@ -58,21 +68,26 @@ if(isset($_POST['submit'])) {
         <!-- Headings for the form -->
         <div class="headingsContainer">
             <h3>Sign In</h3>
-            <p>Sign in with your email and password</p>
+            <p>Sign in with candidate id, your email and password</p>
         </div>
 
         <!-- Main container for all inputs -->
         <div class="mainContainer">
+            <!-- Candidate ID input -->
+            <label for="candidate_ID">Your Candidate ID</label>
+            <input type="text" placeholder="Enter Candidate ID" name="candidate_ID" required>
+
             <!-- Email input -->
-              <label for="email">Your Email</label>
+            <label for="email">Your Email</label>
             <input type="text" placeholder="Enter Email" name="email" required>
 
             <br><br>
 
             <!-- Password input -->
-            <label for = "password">Your Password</label>
+            <label for="password">Your Password</label>
             <input type="password" name="password" placeholder="Password">
             <br><br>
+
             <!-- Submit button -->
             <button type="submit" name="submit" value="Sign In">Submit</button>
         </div>
